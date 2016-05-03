0e207d7ef5111ab4d8e22511a71d2447db1e543f
Jacob Burden <Jacob.JW.Burden@gmail.com>
2016-05-03 18:27:28 -0400

    updates .revision on production after all files are finsihed updating, also moves previous commit hash to .revision-old as a safety net.  Formated to eslint.

--- gulp/tasks/ftp-deploy.js
+++ gulp/tasks/ftp-deploy.js
@@ -1,97 +1,127 @@
 var config = require('../config'),
-  gulp = require('gulp'),
-  fs = require('fs'),
-  path = require('path'),
-  spawn = require('child_process').spawn,
-  exec = require('child_process').exec,
-  concat = require('concat-stream'),
-  minimist = require('minimist'),
-  fromString = require('from2-string'),
-  Client = require('ssh2').Client;
+	gulp = require('gulp'),
+	fs = require('fs'),
+	path = require('path'),
+	spawn = require('child_process').spawn,
+	exec = require('child_process').exec,
+	concat = require('concat-stream'),
+	minimist = require('minimist'),
+	fromString = require('from2-string'),
+	Client = require('ssh2').Client;
+
+var state = {
+	files: [],
+	file: '',
+	filesRemaining: 0,
+	sftp: null,
+	hash: {
+		production: '',
+		local: ''
+	}
+}
 
 var argv = minimist(process.argv.slice(2), {
-  alias: {
-   h: 'host',
-   p: 'port',
-   u: 'username',
-   'pass': 'password' 
-  }
+	alias: {
+		h: 'host',
+		p: 'port',
+		u: 'username',
+		'pass': 'password'
+	}
 });
 
 function ftpDeployTask() {
-  var conn = new Client();
-  conn.on('ready', function() {
-    conn.sftp(sftpConnected);
-  }).connect({
-    host: argv.host,
-    port: argv.port,
-    username: argv.username,
-    password: argv.password
-  });
+	var conn = new Client();
+	conn.on('ready', function() {
+		conn.sftp(sftpConnected);
+	}).connect({
+		host: argv.host,
+		port: argv.port,
+		username: argv.username,
+		password: argv.password
+	});
 }
 
 function sftpConnected(err, sftp) {
-  if (err) throw err;
-  var stream = sftp.createReadStream('.revision');
-  stream.pipe(concat(handleRevision));
+	if (err) throw err;
+	state.sftp = sftp;
+	var stream = sftp.createReadStream('.revision');
+	stream.on('error', function (err) {
+		console.error(err)
+	})
+	stream.pipe(concat(handleRevision));
 }
 
 function handleRevision(buf) {
-  var body = buf.toString();
-  var remoteCommit = body
-  var localCommit = spawn('git', ['rev-parse', 'HEAD']);
-  localCommit.stdout.pipe(concat(function (buf) {
-    var hash = buf.toString();
-    if (hash !== remoteCommit) {
-      fromString(hash).pipe(sftp.createWriteStream('.revision'))
-      var diff = 'git diff --name-status ' + remoteCommit + ' ' + hash;
-      exec(diff, function (err, stdout, stderr) {
-        var files = stdout.split('\n');
-        files.pop();
-        files.forEach(function sftpFile(stat) {
-          var file = stat.split('\t')[1];
-          var status = stat.split('\t')[0];
-          if (status !== 'D') { 
-            if (file.indexOf('/') >= -1) {
-              var dir = path.dirname(file);
-              var i = 0;
-              var parents = dir.split('/');
-              var parent = parents[i];
-              sftpMkdirp();
-              function sftpMkdirp() {
-                sftp.opendir(dir, function (err, buf) {
-                  if (err) {
-                    // parent(s) don't exist
-                    sftp.mkdir(parent, function (err) {
-                      if (err) console.error(err);
-                      i++;
-                      parent += '/' + parents[i];
-                      sftpMkdirp(sftp, dir);
-                    })
-                  } else {
-                    sftpFile(sftp, file);
-                  } 
-                })
-              }  
-            } else {
-              sftpFile(sftp, file);
-            }
-          }
-        });
-      });
-    }    
-  }));
+	state.hash.production = buf.toString();
+	var localCommit = spawn('git', ['rev-parse', 'HEAD']);
+	localCommit.stdout.pipe(concat(compareHashes));
 }
 
-
-function sftpFile(sftp, file) {
-  var rs = fs.createReadStream(file);
-  var ws = sftp.createWriteStream(file);
-  rs.pipe(ws);
-  ws.on('finish', function() {
-    console.log(file, 'is sftp\'d!');
-  })
+function compareHashes (buf) {
+	var hash = buf.toString();
+	state.hash.local = hash;
+	if (state.hash.local !== state.hash.production) {
+		  var diff = 'git diff --name-status ' +
+		  	state.hash.production + ' ' + hash;
+		  exec(diff, sftpDiffedFiles);
+	}
 }
 
-gulp.task( 'deploy', ftpDeployTask);
+function sftpDiffedFiles(err, stdout, stderr) {
+	var files = stdout.split('\n');
+	files.pop();
+	state.files = files;
+	state.filesRemaining = files.length;
+	files.forEach(sftpFile);
+}
+
+function sftpFile(stat) {
+	var file = stat.split('\t')[1];
+	state.file = file;
+	var status = stat.split('\t')[0];
+	if (status !== 'D') {
+		if (file.indexOf('/') >= -1) {
+			var dir = path.dirname(file);
+			var i = 0;
+			var parents = dir.split('/');
+			var parent = parents[i];
+			sftpMkdirp(dir, parents, parent, i);
+		} else {
+			sftpFile(file);
+		}
+	}
+}
+
+function sftpMkdirp(dir, parents, currentParent, index) {
+	sftp.opendir(dir, function (err, buf) {
+		if (err) {
+		    // parent(s) don't exist
+			sftp.mkdir(currentParent, function (err) {
+				if (err) console.error(err);
+				index++;
+				currentParent += '/' + parents[i];
+				sftpMkdirp(dir, parents, currentParent, index);
+			})
+		} else sftpFile(state.file);
+	})
+}
+
+function sftpFile(file) {
+	var rs = fs.createReadStream(file);
+	var ws = state.sftp.createWriteStream(file);
+	rs.pipe(ws);
+	ws.on('finish', function() {
+		console.log(file, 'is sftp\'d!');
+		state.filesRemaining--;
+		if (!state.filesRemaining) {
+			fromString(remoteCommit)
+				.pipe(state.sftp.createWriteStream('.revision-old'))
+			fromString(hash)
+				.pipe(state.sftp.createWriteStream('.revision'))
+			console.log('.revision updated:', hash)
+		}
+	})
+}
+
+gulp.task('deploy', ftpDeployTask);
 module.exports = ftpDeployTask;
