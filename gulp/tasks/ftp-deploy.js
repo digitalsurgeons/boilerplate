56963739dbec3c03de9b22e2708a58a66547f978
Jacob Burden <Jacob.JW.Burden@gmail.com>
2016-05-05 12:14:56 -0400

    pushes all the things

--- gulp/tasks/ftp-deploy.js
+++ gulp/tasks/ftp-deploy.js
@@ -7,6 +7,7 @@ var config = require('../config'),
 	concat = require('concat-stream'),
 	minimist = require('minimist'),
 	fromString = require('from2-string'),
+	recursive = require('recursive-readdir'),
 	Client = require('ssh2').Client;
 
 var state = {
@@ -15,7 +16,8 @@ var state = {
 	hash: {
 		production: '',
 		local: ''
-	}
+	},
+	includes: []
 }
 
 var argv = minimist(process.argv.slice(2), {
@@ -30,6 +32,7 @@ var argv = minimist(process.argv.slice(2), {
 function ftpDeployTask() {
 	var conn = new Client();
 	conn.on('ready', function() {
+		console.log('SFTP CLIENT :: CONNECTED')
 		conn.sftp(sftpConnected);
 	}).connect({
 		host: argv.host,
@@ -43,11 +46,48 @@ function sftpConnected(err, sftp) {
 	if (err) throw err;
 	state.sftp = sftp;
 	var stream = sftp.createReadStream('.revision');
-	stream.on('error', function (err) {
-		console.log('no .revision file found, creating one for you')
-		handleRevision(null)
-	})
+	stream.on('error', handleNoRevision)
 	stream.pipe(concat(handleRevision));
+	var includes = argv.include.split(',');
+	includes.forEach(handleIncludes)
+
+}
+
+function handleNoRevision (err) {
+	console.log('no .revision file found, creating one for you and sftp-ing all the files')
+	recursive('public_html', ['.git', 'node_modules'], function (err, files) {
+		state.filesRemaining += files.length
+		var localCommit = spawn('git', ['rev-parse', 'HEAD']);
+		localCommit.stdout.on('data', function (buf) {
+			var hash = buf.toString();
+			state.hash.local = hash;
+			console.log('hash', hash)
+			files.forEach(ignoreOrSftp);
+		});
+	});
+}
+
+function handleIncludes (file) {
+	if (file.indexOf('/') >= 0) {
+		var dir = file.split('/')[1]
+		var i = 0;
+		var parents = dir.split('/');
+		var parent = parents[i];
+		recursive(dir, function (err, files) {
+			if (err) console.error(err)
+			state.filesRemaining += files.length
+			files.forEach(function (file) {
+				state.includes.push(file);
+				sftpMkdirp(dir, parents, parent, file, i, function () {
+					ignoreOrSftp(file);
+				});
+			});
+		});
+	} else {
+		state.includes.push(file)
+		state.filesRemaining++
+		sftpFile(file);
+	}
 }
 
 function handleRevision(buf) {
@@ -61,7 +101,7 @@ function compareHashes (buf) {
 	state.hash.local = hash;
 	if (state.hash.local !== state.hash.production) {
 		  var diff = 'git diff --name-status ' +
-		  	state.hash.production + ' ' + hash;
+		  state.hash.production + ' ' + hash;
 		  exec(diff, sftpDiffedFiles);
 	}
 }
@@ -69,7 +109,7 @@ function compareHashes (buf) {
 function sftpDiffedFiles(err, stdout, stderr) {
 	var files = stdout.split('\n');
 	files.pop();
-	state.filesRemaining = files.length;
+	state.filesRemaining += files.length;
 	files.forEach(getFileStat);
 }
 
@@ -77,19 +117,18 @@ function getFileStat(stat) {
 	var file = stat.split('\t')[1];
 	var status = stat.split('\t')[0];
 	if (status !== 'D') {
-		if (file.indexOf('/') >= -1) {
-			var dir = path.dirname(file);
-			var i = 0;
-			var parents = dir.split('/');
-			var parent = parents[i];
-			sftpMkdirp(dir, parents, parent, file, i);
-		} else {
-			sftpFile(file);
-		}
+		checkIfDir(file)
 	}
 }
 
-function sftpMkdirp(dir, parents, currentParent, file, index) {
+function sftpMkdirp(dir, parents, currentParent, file, index, cb) {
+	var hasPublicHtml = parents.indexOf('public_html') === 0;
+	if (hasPublicHtml) {
+		parents.shift();
+		currentParent = parents[0];
+		dir = dir.replace('public_html/','');
+	}
+
 	state.sftp.opendir(dir, function (err, buf) {
 		if (err) {
 		    // parent(s) don't exist
@@ -99,25 +138,61 @@ function sftpMkdirp(dir, parents, currentParent, file, index) {
 				currentParent += '/' + parents[index];
 				sftpMkdirp(dir, parents, currentParent, file, index);
 			})
-		} else sftpFile(file);
+		} else {
+			if (cb) {
+				cb();
+			} else sftpFile(file);
+		}
 	})
 }
 
 function sftpFile(file) {
-	var rs = fs.createReadStream(file);
-	var ws = state.sftp.createWriteStream(file);
-	rs.pipe(ws);
-	ws.on('finish', function() {
-		console.log(file, 'is sftp\'d!');
-		state.filesRemaining--;
-		if (!state.filesRemaining) {
-			fromString(state.hash.production)
-				.pipe(state.sftp.createWriteStream('.revision-old'))
-			fromString(state.hash.local)
-				.pipe(state.sftp.createWriteStream('.revision'))
-			console.log('.revision updated:', state.hash.local)
-		}
-	})
+	if (file.indexOf('node_modules') >= 0 || file.indexOf('.git') >= 0) {
+		return;
+	}
+	var isInPublicHtml = file.indexOf('public_html') >= 0
+	var isInIncludes = argv.include && state.includes.indexOf(file) >= 0
+	if (isInPublicHtml || isInIncludes) {
+		var destFile = isInPublicHtml
+			? file.replace('public_html/', '')
+			: file
+
+		var rs = fs.createReadStream(file);
+		var ws = state.sftp.createWriteStream(destFile);
+		rs.pipe(ws);
+		ws.on('finish', function() {
+			console.log(destFile, 'is sftp\'d!');
+			state.filesRemaining--;
+			if (!state.filesRemaining) {
+				fromString(state.hash.production)
+					.pipe(state.sftp.createWriteStream('.revision-old'))
+				fromString(state.hash.local)
+					.pipe(state.sftp.createWriteStream('.revision'))
+				console.log('.revision updated:', state.hash.local)
+			}
+		})
+	}
+}
+
+function checkIfDir(file) {
+	if (file.indexOf('/') >= 0) {
+		var dir = path.dirname(file);
+		var i = 0;
+		var parents = dir.split('/');
+		var parent = parents[i];
+		sftpMkdirp(dir, parents, parent, file, i);
+	} else {
+		sftpFile(file);
+	}
+}
+
+function ignoreOrSftp(file) {
+	if (file.indexOf('.git') >= 0 || file.indexOf('node_modules') >= 0) {
+		console.log('skipping', file);
+		return;
+	} else {
+		checkIfDir(file);
+	}
 }
 
 gulp.task('deploy', ftpDeployTask);
