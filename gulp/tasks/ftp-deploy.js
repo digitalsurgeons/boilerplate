a6ea753d4e603bd9c014a2a682cb661cbe01f6b5
AaronShea <aaron@aaronshea.me>
2016-05-18 15:24:00 -0400

    working on fixing sftp task, collects files according to root

--- gulp/tasks/ftp-deploy.js
+++ gulp/tasks/ftp-deploy.js
@@ -2,7 +2,7 @@ var config = require('../config'),
 	gulp = require('gulp'),
 	fs = require('graceful-fs'),
 	path = require('path'),
-	spawn = require('child_process').spawn,
+	spawn = require('child_process').execFileSync,
 	exec = require('child_process').exec,
 	concat = require('concat-stream'),
 	minimist = require('minimist'),
@@ -13,11 +13,13 @@ var config = require('../config'),
 var state = {
 	filesRemaining: 0,
 	sftp: null,
+	doneCb: null,
+	localRoot: null,
 	hash: {
 		production: '',
 		local: ''
 	},
-	includes: []
+	files: []
 }
 
 var argv = minimist(process.argv.slice(2), {
@@ -25,175 +27,182 @@ var argv = minimist(process.argv.slice(2), {
 		h: 'host',
 		p: 'port',
 		u: 'username',
+		r: 'root',
 		'pass': 'password'
 	}
 });
 
-function ftpDeployTask() {
+function ftpDeployTask(taskDoneCb) {
+
+	// Keep this for when we are done uploading stuff
+	state.doneCb = taskDoneCb;
+
 	var conn = new Client();
 	conn.on('ready', function() {
-		console.log('SFTP CLIENT :: CONNECTED')
 		conn.sftp(sftpConnected);
 	}).connect({
 		host: argv.host,
 		port: argv.port,
 		username: argv.username,
-		password: argv.password
+		password: argv.password,
 	});
 }
 
 function sftpConnected(err, sftp) {
 	if (err) throw err;
-	state.sftp = sftp;
-	var stream = sftp.createReadStream('.revision');
-	stream.on('error', handleNoRevision)
-	stream.pipe(concat(handleRevision));
-	if (argv.include) {
-		var includes = argv.include.split(',');
-		includes.forEach(handleIncludes)
-	}
-}
 
-function handleNoRevision (err) {
-	console.log('no .revision file found, creating one for you and sftp-ing all the files')
-	recursive('public_html', ['.git', 'node_modules'], function (err, files) {
-		state.filesRemaining += files.length
-		var localCommit = spawn('git', ['rev-parse', 'HEAD']);
-		localCommit.stdout.on('data', function (buf) {
-			var hash = buf.toString();
-			state.hash.local = hash;
-			console.log('hash', hash)
-			files.forEach(ignoreOrSftp);
-		});
+	console.log('SFTP CLIENT :: CONNECTED')
+
+	state.sftp = sftp;
+
+	// Try and read the revison file from the server
+	var stream = sftp.createReadStream('.revision');
+
+	// We could not open the revision file, assume this is a new server?
+	stream.on('error', function(){
+		handleNoRevision(collectFiles);
+	});
+
+	// Found a revision file, grab the buffer and go!
+	stream.on('data', function(buffer){
+		handleRevision(buffer);
 	});
 }
 
-function handleIncludes (file) {
-	if (file.indexOf('/') >= 0) {
-		var dir = file.split('/')[1]
-		var i = 0;
-		var parents = dir.split('/');
-		var parent = parents[i];
-		recursive(dir, function (err, files) {
-			if (err) console.error(err)
-			state.filesRemaining += files.length
-			files.forEach(function (file) {
-				state.includes.push(file);
-				sftpMkdirp(dir, parents, parent, file, i, function () {
-					ignoreOrSftp(file);
-				});
-			});
-		});
-	} else {
-		state.includes.push(file)
-		state.filesRemaining++
-		sftpFile(file);
+function handleNoRevision(cb) {
+	console.log('no .revision file found, creating one for you and sftp-ing all the files');
+
+	// Execute git to get the current hash
+	var localCommit = spawn('git', ['rev-parse', 'HEAD']).toString();
+	console.log('hash', localCommit);
+	state.hash.local = localCommit;
+
+	// We can now just collect our files and upload ALL THE THINGS!
+	cb();
+}
+
+// Last step done before trying to upload stuff!
+function collectFiles() {
+
+	console.log('going to collect files now...');
+
+	// Set the local root, where files will be taken from
+	if (argv.root)
+	{
+		if (argv.root[argv.root.length-1] !== '/')
+		{
+			throw 'Invalid root path option! Should be formatted like "public_html/"';
+			return;
+		}
+		state.localRoot = argv.root;
 	}
+
+	// We don't have a revision from the server, so collect all the files
+	if (state.hash.production === ''){
+		console.log('collecting ALL files to upload...');
+		var public_html_files = spawn('find', [
+			state.localRoot || '.',
+			'-not', '-iwholename', '*.git*',
+			'-not', '-iwholename', '*node_modules*',
+			'-printf', '%P\n']).toString().split('\n');
+		state.files = state.files.concat(public_html_files);
+	}
+
+	// Ready to upload files!
+	state.filesRemaining = state.files.length;
+	console.log('about to deploy [' + state.filesRemaining  + '] files!');
+
+	state.files.forEach(sftpFile);
 }
 
 function handleRevision(buf) {
-	state.hash.production = buf.toString();
+	// Save the remote hash
+	state.hash.production = buf.toString().trim();
+
 	var localCommit = spawn('git', ['rev-parse', 'HEAD']);
-	localCommit.stdout.pipe(concat(compareHashes));
-}
+	state.hash.local = localCommit.toString().trim();
 
-function compareHashes (buf) {
-	var hash = buf.toString();
-	state.hash.local = hash;
-	if (state.hash.local !== state.hash.production) {
-		  var diff = 'git diff --name-status ' +
-		  state.hash.production + ' ' + hash;
-		  exec(diff, sftpDiffedFiles);
+	console.log(state.hash.local + ' :: ' + state.hash.production);
+
+	// Do the hashes match?
+	if (state.hash.local !== state.hash.remote) {
+		// They don't so we need to run a diff and find out which files changed
+		console.log('local hash does not match remote hash, deploying changed files');
+		var results = spawn('git', ['diff', '--name-status', state.hash.production, state.hash.local]).toString();
+		console.log(results);
+		results.split('\n').forEach(function(compare){
+			// Check the git status of the file
+
+			var fileRet = getFileStat(compare);
+
+			// Toss it into the file array if it needs to be uploaded
+			if (fileRet !== undefined) {
+				// Notify user about the changed file
+				console.log('[' + fileRet + '] was modified and will be uploaded');
+
+				// Append this to the files to upload
+				state.files.push(fileRet);
+			}
+		});
+
+		// Collect other files that we may want to upload
+		collectFiles();
+	} else {
+		console.log('local and remote hash matches, no need to deploy');
+		process.exit(0);
 	}
-}
-
-function sftpDiffedFiles(err, stdout, stderr) {
-	var files = stdout.split('\n');
-	files.pop();
-	state.filesRemaining += files.length;
-	files.forEach(getFileStat);
 }
 
 function getFileStat(stat) {
+	// Checks if a git diff line is a delete line
+	// if so, ignore this file, don't touch it!
 	var file = stat.split('\t')[1];
 	var status = stat.split('\t')[0];
 	if (status !== 'D') {
-		checkIfDir(file)
-	}
-}
-
-function sftpMkdirp(dir, parents, currentParent, file, index, cb) {
-	var hasPublicHtml = parents.indexOf('public_html') === 0;
-	if (hasPublicHtml) {
-		parents.shift();
-		currentParent = parents[0];
-		dir = dir.replace('public_html/','');
+		return file;
 	}
 
-	state.sftp.opendir(dir, function (err, buf) {
-		if (err) {
-		    // parent(s) don't exist
-			state.sftp.mkdir(currentParent, function (err) {
-				if (err) console.error(err);
-				index++;
-				currentParent += '/' + parents[index];
-				sftpMkdirp(dir, parents, currentParent, file, index);
-			})
-		} else {
-			if (cb) {
-				cb();
-			} else sftpFile(file);
-		}
-	})
+	return null;
 }
 
 function sftpFile(file) {
-	if (file.indexOf('node_modules') >= 0 || file.indexOf('.git') >= 0) {
+
+	// If we get no file, an empty path, or a directory. Ignore.
+	if (!file || file == '' || fs.lstatSync(file).isDirectory())
+	{
 		return;
 	}
-	var isInPublicHtml = file.indexOf('public_html') >= 0
-	var isInIncludes = argv.include && state.includes.indexOf(file) >= 0
-	if (isInPublicHtml || isInIncludes) {
-		var destFile = isInPublicHtml
-			? file.replace('public_html/', '')
-			: file
 
-		var rs = fs.createReadStream(file);
-		var ws = state.sftp.createWriteStream(destFile);
-		rs.pipe(ws);
-		ws.on('finish', function() {
-			console.log(destFile, 'is sftp\'d!');
-			state.filesRemaining--;
-			if (!state.filesRemaining) {
-				fromString(state.hash.production)
-					.pipe(state.sftp.createWriteStream('.revision-old'))
-				fromString(state.hash.local)
-					.pipe(state.sftp.createWriteStream('.revision'))
-				console.log('.revision updated:', state.hash.local)
-			}
-		})
-	}
-}
+	var rs = fs.createReadStream(file);
+	var destFile = file;
 
-function checkIfDir(file) {
-	if (file.indexOf('/') >= 0) {
-		var dir = path.dirname(file);
-		var i = 0;
-		var parents = dir.split('/');
-		var parent = parents[i];
-		sftpMkdirp(dir, parents, parent, file, i);
-	} else {
-		sftpFile(file);
+	if (state.localRoot !== undefined)
+	{
+		// We have a specified root, trim from the destination
+		destFile = file.replace(state.localRoot, '');
 	}
-}
 
-function ignoreOrSftp(file) {
-	if (file.indexOf('.git') >= 0 || file.indexOf('node_modules') >= 0) {
-		console.log('skipping', file);
-		return;
-	} else {
-		checkIfDir(file);
-	}
+	var ws = state.sftp.createWriteStream(file);
+
+	rs.pipe(ws);
+	ws.on('finish', function() {
+		state.filesRemaining--;
+
+		// Let them know we uploaded the file
+		console.log(file + ' -> ' + destFile);
+		// No more files to upload?
+		if (!state.filesRemaining) {
+			// Update the hash on the server
+			fromString(state.hash.production)
+				.pipe(state.sftp.createWriteStream('.revision-old'))
+			fromString(state.hash.local)
+				.pipe(state.sftp.createWriteStream('.revision'))
+			console.log('.revision updated:', state.hash.local);
+
+			// Tell Gulp we are all done
+			state.doneCb();
+		}
+	});
 }
 
 gulp.task('deploy', ftpDeployTask);
