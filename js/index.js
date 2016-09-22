8d274e5636d23e6fafe1dddaeefab6c43c4d84ae
jekrb <Jacob.JW.Burden@gmail.com>
2016-09-21 20:09:19 -0400

    update scss lint

--- js/index.js
+++ js/index.js
@@ -2,9 +2,9 @@ window.jQuery = window.$ = require('jquery');
 const FastClick = require('fastclick');
 
 if ('addEventListener' in document) {
-    document.addEventListener('DOMContentLoaded', () => {
-        FastClick.attach(document.body);
-    }, false);
+	document.addEventListener('DOMContentLoaded', () => {
+		FastClick.attach(document.body);
+	}, false);
 }
 
 const ui = {
