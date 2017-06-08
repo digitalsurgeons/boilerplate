3281ca00b3ee62e9fd2bcfae89118d5f3e5f4bd8
Adam <adam.soffer@digitalsurgeons.com>
2017-06-08 16:03:27 -0400

    Implement header component

--- components/header/index.js
+++ components/header/index.js
@@ -1,4 +1,5 @@
 import $ from 'jquery'
+import Headroom from 'headroom.js'
 
 class Header {
   constructor (element, options) {
@@ -10,16 +11,39 @@ class Header {
     if (!this.element.length) {
       return
     }
-    this.sayHello(this.message)
+    this.initHeadroom()
   }
 
-  sayHello (greeting = 'sup?') {
-    $(this.element).html(greeting)
+  initHeadroom () {
+    const headroom = new Headroom(this.element[0], {
+      classes: {
+        // when element is initialised
+        initial: 'header',
+        // when scrolling up
+        pinned: 'header--pinned',
+        // when scrolling down
+        unpinned: 'header--unpinned',
+        // when above offset
+        top: 'header--top',
+        // when below offset
+        notTop: 'header--notTop',
+        // when at bottom of scoll area
+        bottom: 'header--bottom',
+        // when not at bottom of scroll area
+        notBottom: 'header--notBottom'
+      },
+      tolerance: {
+        down: 10,
+        up: 20
+      }
+    })
+
+    headroom.init()
   }
 }
 
 const header = new Header('.header', {
-  message: 'hello!'
+  hamburger: 'header__hamburger!'
 })
 
 header.init()
---
+++ js/polyfills.js
@@ -0,0 +1,4 @@
+import 'babel-polyfill'
+import objectFitImages from 'object-fit-images'
+
+objectFitImages()
